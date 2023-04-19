465a837b367c717c805825c15624867d84ff6e68
DanielHwang <cksgh0316@gmail.com>
2023-04-19 14:21:43 +0900

    Initial Commit

---
+++ PFEndPointSecurityAppTemp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PFEndPointSecurityAppTemp
+//
+//  Created by jiran_daniel on 2023/04/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
