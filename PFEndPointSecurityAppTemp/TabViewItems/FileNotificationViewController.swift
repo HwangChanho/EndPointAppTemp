30a1d34733fe2f42bf354f67bd1f8ba47b260a45
AlexHwang <cksgh0316@gmail.com>
2023-04-19 22:26:49 +0900

    test commit

--- PFEndPointSecurityAppTemp/TabViewItems/FileNotificationViewController.swift
+++ PFEndPointSecurityAppTemp/TabViewItems/FileNotificationViewController.swift
@@ -10,7 +10,7 @@ import Cocoa
 class FileNotificationViewController: NSViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() 
 
         // Do any additional setup after loading the view.
     }
